298f7b9f544717ee103d46f619f19c0a300de52d
liammtech <liammtech@gmail.com>
2023-11-05 16:24:34 +0000

    Create script module skeleton

---
+++ script.js
@@ -0,0 +1,19 @@
+(function() {
+    var app = {
+        init: function() {
+            this.cacheDom();
+            this.eventListeners();
+            this.render();
+        },
+        cacheDom: function() {
+
+        },
+        eventListeners: function() {
+
+        },
+        render: function() {
+
+        }
+    }
+    app.init();
+})();
