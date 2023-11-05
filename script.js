b501beef8c441bb1f7590a65c99ffed82ca320c5
liammtech <liammtech@gmail.com>
2023-11-05 16:37:25 +0000

    Add pseudocode

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/*
+    1. Render noughts and crosses on page
+    2. Render message on page
+    3. Check for winning condition (total of 8 possible winning conditions: 3 row, 3 columns, 2 diagonals)
+    4. Render winning message: player 1 win, player 2 win, or tie
+    5. Render winning line (if game is won)
+    6. Reset game
+*/
+
 (function() {
     var app = {
         init: function() {
