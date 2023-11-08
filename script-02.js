9f023e73cd03af885026c01dfe19dcf5ea469148
liammtech <liammtech@gmail.com>
2023-11-08 22:56:35 +0000

    Add takeTurn function to player object

--- script-02.js
+++ script-02.js
@@ -1,19 +1,33 @@
 (function() {
     var app = {
+
         init: function() {
             this.game();
             this.gameboard();
             this.playerOne = new this.player("X");
-            this.playerTwo = new this.player("Y");
+            this.playerTwo = new this.player("O");
         },
-        game: {},
-        gameboard: [
-            "","","","","","","","",""
-        ],
+
+        game: function() {
+            this.startingTurn = "X";
+            
+
+        },
+
+        gameboard: ["","","","","","","","",""],
+
         player: function(symbol) {
             this.symbol = symbol;
             this.cells = [];
+            this.takeTurn = function() {
+                choice = prompt("Enter cell no.: ");
+                gameboard[choice] = this.symbol;
+                this.cells.push(choice);
+            }
         }
+
     }
+
     app.init();
+
 })()
