957f40275fd968e87bed0e8ded72709ca59f7770
liammtech <liammtech@gmail.com>
2023-11-10 08:31:07 +0000

    Let's see what happens

--- script-02.js
+++ script-02.js
@@ -1,5 +1,3 @@
-const prompt = require('prompt-sync')();
-
 (function() {
 
     var app = {
@@ -10,6 +8,7 @@ const prompt = require('prompt-sync')();
             this.playerOne = new this.player("X");
             this.playerTwo = new this.player("Y");
         },
+        
         game: {},
         gameboard: [
             "","","","","","","","",""
