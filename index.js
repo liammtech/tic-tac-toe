1f665cfe29f08681c6ba3b31a46afbec9673578d
liam <liammtech@gmail.com>
2025-09-01 21:39:43 +0100

    Add initial console printing logic

--- index.js
+++ index.js
@@ -8,5 +8,21 @@ const gameboard = (function () {
 })();
 
 const shellDisplayController = (function () {
+    const printBoard = function () {
+        const [ a, b, c, d, e, f, g, h, i ] = gameboard.getBoard();
 
+        console.log(
+            `   |   |   `, `\n`,
+            ` ${a} | ${b} | ${c} `, `\n`,
+            `___|___|___`, `\n`,
+            `   |   |   `, `\n`,
+            ` ${d} | ${e} | ${f} `, `\n`,
+            `___|___|___`, `\n`,
+            `   |   |   `, `\n`,
+            ` ${g} | ${h} | ${i} `, `\n`,
+            `   |   |   `
+        );
+    };
+
+    return { printBoard };
 })();
